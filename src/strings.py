b75e487633e10a1a579ff6eaba662894921843e6
Francesco Re <francesco.re1107@gmail.com>
2022-03-06 10:26:01 +0100

    Change welcome message

--- src/strings.py
+++ src/strings.py
@@ -1,6 +1,6 @@
 
 class Strings():
-    WELCOME_MESSAGE = "Hi"
+    WELCOME_MESSAGE = "Hi!\n\nThis bot lets you monitor up to 10 hosts. Whenever they go OFFLINE or ONLINE I'll send you a notification."
     CANCEL = "❌ Cancel"
     NEW_WATCHDOG = "➕ New watchdog"
     LIST_WATCHDOGS = "📄 My watchdogs"
